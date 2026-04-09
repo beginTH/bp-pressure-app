6a37dce1903f00d175870c714821927d8e8644cc
beginTH <rkitj@PopMasterPC>
2026-04-09 19:27:30 +0700

    Optimize PWA for APK generation: Improve manifest and SW registration

--- app.js
+++ app.js
@@ -413,7 +413,13 @@ const app = {
 
     registerSW: function() {
         if ('serviceWorker' in navigator) {
-            navigator.serviceWorker.register('sw.js').catch(err => console.log(err));
+            navigator.serviceWorker.register('./sw.js', { scope: './' })
+                .then(reg => {
+                    console.log('SW Registered with scope:', reg.scope);
+                    // Explicitly update to help PWABuilder/Browsers detect the latest logic
+                    reg.update();
+                })
+                .catch(err => console.log('SW Error:', err));
         }
     }
 };
